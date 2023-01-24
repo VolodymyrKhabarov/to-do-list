bc50733ab395f0e098f390b77b28a96a2079f428
“Volodymyr <vladymyr.khabarov@gmail.com>
2023-01-24 11:04:04 +0200

    додав get маршрут та оновив patch маршрут

--- server/index.js
+++ server/index.js
@@ -10,21 +10,27 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res, next) => {
-    res.json({msg: "Hi to my first server"});
+    res.json({ msg: "Hi to my first server" });
 });
 
 let id = 1;
 
+app.get('/todo' , (req , res)=>{
+    let rawdata = fs.readFileSync("db.json");
+    let data = JSON.parse(rawdata);
+    res.send(data);
+});
+
 app.post("/todo", (req, res) => {
-    const {title, body, completed} = req.body;
+    const { title, body, completed } = req.body;
     let rawdata = fs.readFileSync("db.json");
     let content = JSON.parse(rawdata);
     if (!title) {
-        return res.status(400).json({message: "No title in your request"});
+        return res.status(400).json({ message: "No title in your request" });
     }
 
     if (!body) {
-        return res.status(400).json({message: "No body in your request"});
+        return res.status(400).json({ message: "No body in your request" });
     }
 
     const todo = {
@@ -45,11 +51,11 @@ app.post("/todo", (req, res) => {
 });
 
 app.delete("/todo/:id", (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     let rawdata = fs.readFileSync("db.json", "utf8");
     let content = JSON.parse(rawdata);
     if (!content.find((i) => i.id == id)) {
-        return res.status(404).json({message: "Todo with that id not found"});
+        return res.status(404).json({ message: "Todo with that id not found" });
     } else {
         const toWrite = content.filter((i) => i.id != id);
 
@@ -58,25 +64,38 @@ app.delete("/todo/:id", (req, res) => {
                 console.error(err);
             }
         });
-        res.status(202).json({message: "Successfully deleted"});
+        res.status(202).json({ message: "Successfully deleted" });
     }
 });
 
 app.patch("/todo/:id", (req, res) => {
-    const {id} = req.params;
+    console.log('req', req)
+    console.log("req.params --->", req.params);
+    const { id } = req.params;
+
     let rawdata = fs.readFileSync("db.json", "utf8");
+    console.log("rawdata --->", rawdata);
+
     let content = JSON.parse(rawdata);
+    console.log("content --->", content);
+
     if (!content.find((i) => i.id == id)) {
-        return res.status(404).json({message: "Todo with that id not found"});
+        return res.status(404).json({ message: "Todo with that id not found" });
     } else {
         const newTodo = req.body;
+        console.log("newTodo1 --->", newTodo);
         const toWrite = content.map((i) => {
-            if (i.id === id) {
+            if (i.id == id) {
+                console.log("newTodo2 --->", newTodo);
+                newTodo['id'] = parseInt(id);
+                newTodo['title'] = i.title;
+                newTodo['body'] = i.body;
                 return newTodo;
             }
+            console.log("i --->", i);
             return i;
         });
-
+        console.log("toWrite --->", toWrite);
         fs.writeFileSync("db.json", JSON.stringify(toWrite), (err) => {
             if (err) {
                 console.error(err);
